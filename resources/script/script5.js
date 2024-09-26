9c9106552e7aa06ba4bf85f86d3f2817fb88c30a
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2024-09-25 20:02:23 -0500

    preventDefault, stopPropagation, stopImmediatePropagation, json, fetch

--- resources/script/script5.js
+++ resources/script/script5.js
@@ -1,11 +1,45 @@
-let a = document.querySelector('input');
-a.addEventListener('click', ()=>{
-    console.log('click en el input');
-    a.classList.forEach((x) => console.log(x));
+let a = document.querySelector("input");
 
-})
+function manjeador1(ev) {
+  console.log("click en el input");
+  a.classList.forEach((x) => console.log(x));
+  ev.stopImmediatePropagation();
+}
 
-document.getElementById('miForm').addEventListener('submit', (ev)=>{
-    console.log('estamos enviando el formulario');
-    ev.preventDefault()
-})
+const manejador2 = function () {
+  console.log("segundo manejador");
+};
+
+a.addEventListener("click", manjeador1);
+
+a.addEventListener("click", manejador2);
+
+
+document.getElementById("miForm").addEventListener("submit", (ev) => {
+  console.log("estamos enviando el formulario");
+  // ev.preventDefault();
+});
+
+let mia = document.getElementById("mia");
+mia.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  console.log(evt);
+});
+
+
+const miMain = document.querySelector('main');
+miMain.addEventListener('click', ()=>{
+    console.log('click en el main');
+});
+
+const miForm = document.querySelector('form');
+miForm.addEventListener('click', (ev)=>{
+    console.log('click en el form');
+    ev.stopPropagation();
+});
+
+const miDIv = document.querySelector('div');
+miDIv.addEventListener('click', (ev)=>{
+    console.log('click en el div');
+    ev.stopPropagation();
+});
