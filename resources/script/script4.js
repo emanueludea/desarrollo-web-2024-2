5b272cffe7dd5ac93d8f92415c7631553b7d5403
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2024-09-27 19:49:06 -0500

    Septiembre27

--- resources/script/script4.js
+++
@@ -1,7 +0,0 @@
-const miBody = document.body;
-const miDiv = document.createElement('div');
-miDiv.classList.add('miClase');
-miBody.appendChild(miDiv);
-miDiv.innerHTML = '<p>asdfkjhasdkfljhaklsjdhflkjhsadflkj</p> <span>hola <em>mundo</em><span>'
-let items = document.getElementsByTagName('li')
-let miLi = document.querySelectorAll('li');
