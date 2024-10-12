a6327d8a16ea037e1d5ad40949498678cc5fceb1
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2024-10-11 19:54:43 -0500

    Agrega dispositivas iniciales de node

--- backend/serverExpress.js
+++ backend/serverExpress.js
@@ -1,29 +1,30 @@
-//const express = require('express')
+//const express = require("express"); //CommonJS
 
-const {default:express} = await import("express");
-const app = express()
+//const {default:express} = await import("express"); // REPL
+import express from "express"; //ES6
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.get('/api', (req, res) => {
-  res.send('hello API')
-})
-
-app.post('/api', (req, res) => {
-  res.send('hello API POST')
-})
-
-app.put('/api', (req, res) => {
-  console.log(req);
-  res.send('hello API PUT')
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
-app.post('/api2', (req, res) => {
+app.get("/api", (req, res) => {
+  res.send("hello API");
+});
+
+app.post("/api", (req, res) => {
+  res.send("hello API POST");
+});
+
+app.put("/api", (req, res) => {
+  console.log(req);
+  res.send("hello API PUT");
+});
+
+app.post("/api2/:id", (req, res) => {
   console.log(req.params);
   console.log(req.query);
-  res.send('hello API2 POST');
+  res.send("hello API2 POST");
 });
 
-app.listen(3000);
+app.listen(8080);
